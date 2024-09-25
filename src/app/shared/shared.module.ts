b1603531795821f565f1538c021fab80c8767029
jeyofdev <jgregoire.dev@gmail.com>
2024-09-25 15:09:38 +0200

    config : init primeng and theme

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { PrimengModule } from './modules/primeng.module';
 
 @NgModule({
-  declarations: [
-    ButtonComponent
-  ],
-  imports: [CommonModule],
-	exports: [ButtonComponent]
+  declarations: [ButtonComponent],
+  imports: [CommonModule, PrimengModule],
+  exports: [ButtonComponent, PrimengModule],
 })
 export class SharedModule {}
