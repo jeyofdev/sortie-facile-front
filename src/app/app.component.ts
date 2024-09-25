543b30443de612602f9f756ade39234ce04fcfe3
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2024-09-25 15:21:02 +0200

    init configuration path, primeng + theme and prettier (#1)
    * config : init architecture
    
    * config : init primeng and theme
    
    * config : init prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sortie-facile-front';
+	title = 'sortie-facile-front';
 }
