fdbf63bf1eaf1653be27a0390755b02b29ad20f0
jeyofdev <jgregoire.dev@gmail.com>
2024-09-26 14:18:24 +0200

    feat : add top header

--- src/app/shared/modules/components.module.ts
+++ src/app/shared/modules/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from '@shared/components/header/header.component';
+import { PrimengModule } from './primeng.module';
 
 @NgModule({
 	declarations: [HeaderComponent],
+	imports: [PrimengModule],
 	exports: [HeaderComponent],
 })
 export class ComponentModule {}
