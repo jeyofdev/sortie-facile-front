b164d24a73f241fada827bdf92ad5389470aa669
jeyofdev <jgregoire.dev@gmail.com>
2024-09-26 16:24:04 +0200

    refacto : add component app-input-search

--- src/app/shared/modules/components.module.ts
+++ src/app/shared/modules/components.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { PrimengModule } from './primeng.module';
 import { ButtonHeaderIconComponent } from '@shared/components/buttons/button-header-icon/button-header-icon.component';
+import { InputSearchComponent } from '@shared/components/form/input/input-search/input-search.component';
 
 @NgModule({
-	declarations: [HeaderComponent, ButtonHeaderIconComponent],
+	declarations: [HeaderComponent, ButtonHeaderIconComponent, InputSearchComponent],
 	imports: [PrimengModule],
 	exports: [HeaderComponent],
 })
