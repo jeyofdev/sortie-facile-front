a455da0be871b8cfd8890f142cbd144015563448
jeyofdev <jgregoire.dev@gmail.com>
2024-09-26 16:31:30 +0200

    refacto : add component app-logo

--- src/app/shared/modules/components.module.ts
+++ src/app/shared/modules/components.module.ts
@@ -3,9 +3,10 @@ import { HeaderComponent } from '@shared/components/header/header.component';
 import { PrimengModule } from './primeng.module';
 import { ButtonHeaderIconComponent } from '@shared/components/buttons/button-header-icon/button-header-icon.component';
 import { InputSearchComponent } from '@shared/components/form/input/input-search/input-search.component';
+import { LogoComponent } from '@shared/components/logo/logo.component';
 
 @NgModule({
-	declarations: [HeaderComponent, ButtonHeaderIconComponent, InputSearchComponent],
+	declarations: [HeaderComponent, ButtonHeaderIconComponent, InputSearchComponent, LogoComponent],
 	imports: [PrimengModule],
 	exports: [HeaderComponent],
 })
