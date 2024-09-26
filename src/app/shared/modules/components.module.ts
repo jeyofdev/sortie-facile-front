884989de08e6a8454dff4424bb8c5acb1caea3ff
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2024-09-26 17:33:23 +0200

    Footer (#6)
    * feat : add footer bottom
    
    * feat : add footer top

--- src/app/shared/modules/components.module.ts
+++ src/app/shared/modules/components.module.ts
@@ -4,10 +4,11 @@ import { PrimengModule } from './primeng.module';
 import { ButtonHeaderIconComponent } from '@shared/components/buttons/button-header-icon/button-header-icon.component';
 import { InputSearchComponent } from '@shared/components/form/input/input-search/input-search.component';
 import { LogoComponent } from '@shared/components/logo/logo.component';
+import { FooterComponent } from '@shared/components/footer/footer.component';
 
 @NgModule({
-	declarations: [HeaderComponent, ButtonHeaderIconComponent, InputSearchComponent, LogoComponent],
+	declarations: [HeaderComponent, ButtonHeaderIconComponent, InputSearchComponent, LogoComponent, FooterComponent],
 	imports: [PrimengModule],
-	exports: [HeaderComponent],
+	exports: [HeaderComponent, FooterComponent],
 })
 export class ComponentModule {}
