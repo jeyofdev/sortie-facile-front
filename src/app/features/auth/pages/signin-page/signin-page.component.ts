7318b9e005c956d8fecce90381e4d8a59fe584af
jeyofdev <jgregoire.dev@gmail.com>
2024-09-28 10:01:56 +0200

    feat : init reactive form login

--- src/app/features/auth/pages/signin-page/signin-page.component.ts
+++ src/app/features/auth/pages/signin-page/signin-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-signin-page',
-  templateUrl: './signin-page.component.html',
-  styleUrl: './signin-page.component.scss'
+	selector: 'app-signin-page',
+	templateUrl: './signin-page.component.html',
+	styleUrl: './signin-page.component.scss',
 })
-export class SigninPageComponent {
+export class SigninPageComponent implements OnInit {
+	formIsSubmitted: boolean = false;
+	loginForm!: FormGroup;
 
+	emailCtrl!: FormControl;
+	passwordCtrl!: FormControl;
+
+	regexEmail!: RegExp;
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.initFormControls();
+		this.initLoginForm();
+	}
+
+	onSubmit(): void {
+		console.log(this.loginForm.value);
+	}
+
+	private initLoginForm() {
+		this.loginForm = this.formBuilder.group({
+			email: this.emailCtrl,
+			password: this.passwordCtrl,
+		});
+	}
+
+	private initFormControls(): void {
+		this.emailCtrl = this.formBuilder.control('');
+		this.passwordCtrl = this.formBuilder.control('');
+	}
 }
--- src/app/shared/modules/primeng.module.ts
+++ src/app/shared/modules/primeng.module.ts
@@ -5,8 +5,9 @@ import { MenuModule } from 'primeng/menu';
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
-	exports: [ButtonModule, DialogModule, MenuModule, InputIconModule, IconFieldModule, InputTextModule],
+	exports: [ButtonModule, DialogModule, MenuModule, InputIconModule, IconFieldModule, InputTextModule, PasswordModule],
 })
 export class PrimengModule {}
