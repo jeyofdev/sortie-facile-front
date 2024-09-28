30f29b990e434d24635a31079faa94870c721537
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2024-09-28 11:27:30 +0200

    page signin (#9)
    * feat : init reactive form login
    
    * feat : add validations in login form
    
    * style : page signin
    
    * style : responsive content page signin

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninPageComponent } from '@auth/pages/signin-page/signin-page.component';
 import { AuthRoutingModule } from '@auth/auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
 	declarations: [SigninPageComponent],
-	imports: [CommonModule, AuthRoutingModule],
+	imports: [CommonModule, AuthRoutingModule, FormsModule, ReactiveFormsModule, SharedModule],
 })
 export class AuthModule {}
--- src/app/features/auth/pages/signin-page/signin-page.component.ts
+++ src/app/features/auth/pages/signin-page/signin-page.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-signin-page',
-  templateUrl: './signin-page.component.html',
-  styleUrl: './signin-page.component.scss'
+	selector: 'app-signin-page',
+	templateUrl: './signin-page.component.html',
+	styleUrl: './signin-page.component.scss',
 })
-export class SigninPageComponent {
+export class SigninPageComponent implements OnInit {
+	formIsSubmitted: boolean = false;
+	loginForm!: FormGroup;
 
+	emailCtrl!: FormControl;
+	passwordCtrl!: FormControl;
+
+	regexEmail!: RegExp;
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.initFormControls();
+		this.initLoginForm();
+	}
+
+	onSubmit(): void {
+		this.formIsSubmitted = true;
+
+		for (const control in this.loginForm.controls) {
+			this.loginForm.controls[control].markAsTouched();
+		}
+
+		console.log(this.loginForm);
+
+		console.log(this.loginForm.value);
+	}
+
+	private initLoginForm() {
+		this.loginForm = this.formBuilder.group({
+			email: this.emailCtrl,
+			password: this.passwordCtrl,
+		});
+	}
+
+	private initFormControls(): void {
+		this.regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+		this.emailCtrl = this.formBuilder.control('', [Validators.required, Validators.pattern(this.regexEmail)]);
+		this.passwordCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(8)]);
+	}
 }
