bcc917a9246f4ca0f076d14fc104f58b323f4817
jeyofdev <jgregoire.dev@gmail.com>
2024-10-15 16:46:47 +0200

    feat : init content page account/settings/contact

--- src/app/features/account/pages/account-settings/account-settings-contact-page/account-settings-contact-page.component.ts
+++ src/app/features/account/pages/account-settings/account-settings-contact-page/account-settings-contact-page.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountSettingsPageAbstract } from '@shared/abstract/account-settings-page.abstract';
+import { FormContact } from '@shared/types/form/form-contact.type';
+import { FormSocial } from '@shared/types/form/form-social.type';
+import { RegexHelper } from '@shared/utils/regex.helper';
+import { validationAccountMessages } from '@shared/validations/messages/account-settings-message.error';
 
 @Component({
-  selector: 'app-account-settings-contact-page',
-  templateUrl: './account-settings-contact-page.component.html',
-  styleUrl: './account-settings-contact-page.component.scss'
+	selector: 'app-account-settings-contact-page',
+	templateUrl: './account-settings-contact-page.component.html',
+	styleUrl: './account-settings-contact-page.component.scss',
 })
-export class AccountSettingsContactPageComponent {
+export class AccountSettingsContactPageComponent extends AccountSettingsPageAbstract<FormContact> implements OnInit {
+	socialForm!: FormGroup<FormSocial>;
 
+	phoneCtrl!: FormControl<string>;
+	twitterCtrl!: FormControl<string>;
+	instagramCtrl!: FormControl<string>;
+	facebookCtrl!: FormControl<string>;
+
+	constructor(private _formBuilder: FormBuilder) {
+		super();
+	}
+
+	override ngOnInit(): void {
+		this.validationMessages = validationAccountMessages;
+		super.ngOnInit();
+	}
+
+	onSubmit(): void {
+		this.formError = '';
+		if (this.mainForm.valid) {
+			console.log(this.mainForm);
+			// TODO save contact and social media
+		} else {
+			this.formError = 'The form contains errors. Please verify your information.';
+		}
+	}
+
+	protected override initMainForm() {
+		this.mainForm = this._formBuilder.group({
+			phone: this.phoneCtrl,
+			socialForm: this.socialForm,
+		});
+	}
+
+	protected override initFormControls(): void {
+		this.phoneCtrl = this._formBuilder.control('', {
+			validators: [Validators.required, Validators.pattern(RegexHelper.phone)],
+			nonNullable: true,
+		});
+		this.twitterCtrl = this._formBuilder.control('', {
+			validators: [Validators.minLength(3), Validators.minLength(3), Validators.maxLength(30)],
+			nonNullable: true,
+		});
+		this.instagramCtrl = this._formBuilder.control('', {
+			validators: [Validators.minLength(3), Validators.minLength(3), Validators.maxLength(30)],
+			nonNullable: true,
+		});
+		this.facebookCtrl = this._formBuilder.control('', {
+			validators: [Validators.minLength(3), Validators.minLength(3), Validators.maxLength(30)],
+			nonNullable: true,
+		});
+
+		this.socialForm = this._formBuilder.group({
+			twitter: this.twitterCtrl,
+			instagram: this.instagramCtrl,
+			facebook: this.facebookCtrl,
+		});
+	}
 }
--- src/app/shared/validations/messages/account-settings-message.error.ts
+++ src/app/shared/validations/messages/account-settings-message.error.ts
@@ -13,4 +13,21 @@ export const validationAccountMessages: ValidationMessages[] = [
 		required: 'Confirm password is required.',
 		minlength: 'Confirm password must be at least 8 characters long.',
 	}),
+	new ValidationMessages('phone', {
+		required: 'The phone is required.',
+		pattern:
+			'The format of the phone number is incorrect. Please enter a valid phone number in the format: 01 99 99 99 99.',
+	}),
+	new ValidationMessages('twitter', {
+		minlength: 'The twitter username must be at least 3 characters long.',
+		maxlength: 'The twitter username must have a maximum 30 characters long.',
+	}),
+	new ValidationMessages('instagram', {
+		minlength: 'The instagram username must be at least 3 characters long.',
+		maxlength: 'The instagram username must have a maximum 30 characters long.',
+	}),
+	new ValidationMessages('facebook', {
+		minlength: 'The facebook username must be at least 3 characters long.',
+		maxlength: 'The facebook username must have a maximum 30 characters long.',
+	}),
 ];
