e7298fa93a807049b0103750f89eb933325aa491
jeyofdev <jgregoire.dev@gmail.com>
2024-10-22 15:01:02 +0200

    feat : show interests user in page settings interests

--- src/app/features/account/pages/account-settings/account-settings-interests-page/account-settings-interests-page.component.ts
+++ src/app/features/account/pages/account-settings/account-settings-interests-page/account-settings-interests-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountAndResult } from '@shared/models/count-and-result.model';
+import { ResponseInterestBase } from '@shared/models/interests/response/response-interest-base.model';
+import { MessageService } from 'primeng/api';
+import { first } from 'rxjs';
 
 @Component({
-  selector: 'app-account-settings-interests-page',
-  templateUrl: './account-settings-interests-page.component.html',
-  styleUrl: './account-settings-interests-page.component.scss'
+	selector: 'app-account-settings-interests-page',
+	templateUrl: './account-settings-interests-page.component.html',
+	styleUrl: './account-settings-interests-page.component.scss',
+	providers: [MessageService],
 })
 export class AccountSettingsInterestsPageComponent {
+	userInterests!: CountAndResult<ResponseInterestBase>;
 
+	constructor(protected _activatedRoute: ActivatedRoute) {}
+
+	ngOnInit(): void {
+		this._activatedRoute.parent?.data.pipe(first()).subscribe(data => {
+			this.userInterests = data['profile']?.categories;
+		});
+
+		console.log(this.userInterests);
+	}
 }
